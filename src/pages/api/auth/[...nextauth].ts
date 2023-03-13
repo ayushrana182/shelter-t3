aa7e3c1e89ea3c2160bc2ba61de90550ade665ed
Ayush Rana <ayushrana182@gmail.com>
2023-03-13 10:53:41 +0545

    fix:env update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import FacebookProvider from "next-auth/providers/facebook";
 
 providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET
     })
   ]
 
