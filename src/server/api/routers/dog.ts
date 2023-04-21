6cc4215fb577a6061d3e9c771bdaa8807fb30363
Ayush Rana <ayushrana182@gmail.com>
2023-04-21 11:12:14 +0545

    feature: prisma schema and trpc routers

---
+++ src/server/api/routers/dog.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const dogRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.dog.findMany();
+  }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        userId: z.string(),
+        breed: z.string(),
+        about: z.string(),
+        status: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.dog.create({
+        data: {
+          name: input.name,
+          userId: ctx.session.user.id,
+          breed: input.breed,
+          about: input.about,
+          status: input.status,
+        },
+      });
+    }),
+});
